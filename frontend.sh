cbc58a543ea2890c5deda3c17afdae149e0e1c16
shivacharan999 <charan02ch@gmail.com>
2023-12-12 08:39:55 -0800

    M

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,33 @@
-dnf install nginx -y 
-rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
-systemctl enable nginx 
-systemctl start nginx 
+source common.sh
 
-## need to copy roboshop config
+echo -e "\e[36m>>>>>>>>>>>>  Install Nginx   <<<<<<<<<<<<\e[0m"
+yum install nginx -y &>>${log}
+func_exit_status
+
+
+echo -e "\e[36m>>>>>>>>>>>>  Copy RoboShop Configuration  <<<<<<<<<<<<\e[0m"
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
+func_exit_status
+
+
+echo -e "\e[36m>>>>>>>>>>>>  Clean Old content  <<<<<<<<<<<<\e[0m"
+rm -rf /usr/share/nginx/html/* &>>${log}
+func_exit_status
+
+
+echo -e "\e[36m>>>>>>>>>>>>  Download Application Content   <<<<<<<<<<<<\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
+func_exit_status
+
+cd /usr/share/nginx/html
+
+echo -e "\e[36m>>>>>>>>>>>>  Extract Application Content  <<<<<<<<<<<<\e[0m"
+unzip /tmp/frontend.zip &>>${log}
+func_exit_status
+
+
+echo -e "\e[36m>>>>>>>>>>>>  Start Nginx Service  <<<<<<<<<<<<\e[0m"
+systemctl enable nginx &>>${log}
+systemctl restart nginx &>>${log}
+func_exit_status
+
