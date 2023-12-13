194a731501ba065eb0b2663c16af981fcc267b31
shivacharan999 <charan02ch@gmail.com>
2023-12-12 22:51:16 -0800

    M

--- frontend.sh
+++ frontend.sh
@@ -4,13 +4,12 @@ dnf install nginx -y
 echo -e "\e[35m <<<<<<< Removing old content >>>>>>> \e[0m"
 rm -rf /usr/share/nginx/html/* 
 
-echo -e "\e[35m <<<<<<< downloading frontend package >>>>>>> \e[0m"
+echo -e "\e[35m <<<<<<< Downloading frontend package >>>>>>> \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 
-echo -e "\e[35m <<<<<<< CD to html dir >>>>>>> \e[0m"
-cd /usr/share/nginx/html 
 
 echo -e "\e[35m <<<<<<< Unzipping the frontend content >>>>>>> \e[0m"
+cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
 echo -e "\e[35m <<<<<<< cp roboshop.conf file to system >>>>>>> \e[0m"
