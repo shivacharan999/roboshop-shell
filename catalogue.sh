516e0fe706fd25931d26e566254112fd5c6b8ce5
shivacharan999 <charan02ch@gmail.com>
2023-12-12 19:38:37 -0800

    M

---
+++ catalogue.sh
@@ -0,0 +1,20 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+useradd roboshop
+mkdir /app 
+rm -rf /app/*
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+cd /app 
+npm install 
+cp configs/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
+
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org-shell -y
+
+mongo --host mongodb.devsig90.com </app/schema/catalogue.js
--- main.tf
+++
@@ -1,8 +0,0 @@
-module "ec2" {
-    for_each = var.instances
-    source = "./ec2"
-    component = each.value["name"]
-    instance_type = each.value["type"]
-    password = try(each.value["password"], "null")
-}
-
