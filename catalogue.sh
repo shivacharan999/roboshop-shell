d3c405d3e6ac5ba6750fd4661605bf309ec96a09
shivacharan999 <charan02ch@gmail.com>
2023-12-12 20:41:18 -0800

    M

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,7 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+curl -sL https:///rpm.nodesource.com/setup_lts.x | bash
 useradd roboshop
 mkdir /app 
+rm -rf /app/*
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
 unzip /tmp/catalogue.zip
