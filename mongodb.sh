385b092decd8f2b92316039a55d765a70ffeefe4
shivacharan999 <charan02ch@gmail.com>
2023-12-13 22:32:33 -0800

    M

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,20 @@
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y 
-systemctl enable mongod 
-systemctl start mongod 
+code_dir=$(pwd)
+log_file=/tmp/robodhop.log
+rm-rf ${log_file}
+
+print_head() {
+    echo -e "\e[36m$1\e[0m"
+}
+
+print_head  " <<<<<<< cp configs/mongodb.repo >>>>>>> "
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+
+print_head  " <<<<<<< install mongodb-org >>>>>>> "
+yum install mongodb-org -y  &>>${log_file}
+
+print_head  " <<<<<<< enable mongod x >>>>>>> "
+systemctl enable mongod  &>>${log_file}
+
+print_head  " <<<<<<< start mongod  >>>>>>> "
+systemctl start mongod  &>>${log_file}
 ##Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
