d4093cbabdb97e5c32b828417cfc2c74ea373b60
shivacharan999 <charan02ch@gmail.com>
2023-12-13 22:44:36 -0800

    M

--- catalogue.sh
+++ catalogue.sh
@@ -1,52 +1,52 @@
 source common.sh
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-curl -sL https:///rpm.nodesource.com/setup_lts.x | bash
+curl -sL https:///rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-useradd roboshop
+useradd roboshop &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-mkdir /app 
+mkdir /app  &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-rm -rf /app/*
+rm -rf /app/* &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-cd /app 
+cd /app &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-cd /app 
+cd /app &>>${log_file}
+ 
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
+npm install &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-npm install 
+cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-systemctl daemon-reload
+systemctl enable catalogue  &>>${log_file}
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-systemctl enable catalogue 
-
-print_head  " <<<<<<< Installing Nginx >>>>>>> "
-systemctl start catalogue
-
-
-print_head  " <<<<<<< Installing Nginx >>>>>>> "
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+systemctl start catalogue &>>${log_file}
 
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-dnf install mongodb-org-shell -y
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-mongo --host 54.204.183.17 </app/schema/catalogue.js
+dnf install mongodb-org-shell -y &>>${log_file}
+
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
+mongo --host 54.204.183.17 </app/schema/catalogue.js &>>${log_file}
