b67de041689897d2d831fb2ed40c1393213b0d98
shivacharan999 <charan02ch@gmail.com>
2023-12-13 22:42:59 -0800

    M

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,52 @@
+source common.sh
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 curl -sL https:///rpm.nodesource.com/setup_lts.x | bash
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 useradd roboshop
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 mkdir /app 
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 rm -rf /app/*
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 cd /app 
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 unzip /tmp/catalogue.zip
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 cd /app 
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 npm install 
-cp configs/catalogue.service /etc/systemd/system/catalogue.service
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
+cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 systemctl daemon-reload
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 systemctl enable catalogue 
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 systemctl start catalogue
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 dnf install mongodb-org-shell -y
 
-mongo --host mongodb.devsig90.com </app/schema/catalogue.js
+
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
+mongo --host 54.204.183.17 </app/schema/catalogue.js
