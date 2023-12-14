861b65602e0b81679a2168539e4fab1c3f307d37
shivacharan999 <charan02ch@gmail.com>
2023-12-14 09:32:13 -0800

    M

--- payment.sh
+++ payment.sh
@@ -2,7 +2,7 @@ source common.sh
 
 roboshop_app_password=$1
 
-if [ -z "${roboshop_app_password}" ]
+if [ -z "${roboshop_app_password}" ]; then ## roboshop123
   echo -e "\e[31mNeed Mysql Root Password\e[0m"
   exit 1
 fi
