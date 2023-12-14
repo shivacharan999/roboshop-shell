251b319c7589451921c854e9ebf0c5a8a030d202
shivacharan999 <charan02ch@gmail.com>
2023-12-14 04:58:50 -0800

    M

--- common.sh
+++ common.sh
@@ -126,13 +126,34 @@ java() {
 
    print_head "download dependencies & Packages"
    mvn clean package &>>{log_file}
-   &>>{log_file}
+   status_check $?
 
    print_head "download dependencies & Packages"
    mv target/${component}-1.0.jar ${component}.jar  &>>{log_file}
-   &>>{log_file}
+  status_check $?
 
    schema_setup
 
    systemd_setup
 }
+
+
+
+python() {
+
+   print_head "install python"
+   dnf install python36 gcc python3-devel -y &>>{log_file}
+   status_check $?
+
+   app_prereq_setup
+
+   print_head "download dependencies & Packages"
+   pip3.6 install -r requirements.txt &>>{log_file}
+   status_check $?
+
+
+   systemd_setup
+}
+
+
+
---
+++ payment.sh
@@ -0,0 +1,11 @@
+source common.sh
+
+roboshop_app_password=$1
+
+if [ -z "${roboshop_app_password}" ]
+  echo -e "\e[31mNeed Mysql Root Password\e[0m"
+  exit 1
+fi
+
+component=payment
+python
