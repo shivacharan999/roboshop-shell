5e9002f52f83360ac0a7aa322c3f01d4bf8d12d2
shivacharan999 <charan02ch@gmail.com>
2023-12-14 06:50:05 -0800

    M

--- redis.sh
+++ redis.sh
@@ -14,7 +14,7 @@ dnf install redis -y &>>${log_file}
 status_check $?
 
 print_head  " <<<<<<< Installing Nginx >>>>>>> "
-sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf&>>${log_file}
 status_check $?
 
 
