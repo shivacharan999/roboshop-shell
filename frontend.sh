df6be0106fe5d1ab56c2bd04e4e7a71b83403d60
shivacharan999 <charan02ch@gmail.com>
2023-12-13 22:26:43 -0800

    M

--- frontend.sh
+++ frontend.sh
@@ -6,25 +6,25 @@ print_head() {
     echo  "\e[36m$1\e[0m"
 }
 
-print_head()  " <<<<<<< Installing Nginx >>>>>>> "
+print_head  " <<<<<<< Installing Nginx >>>>>>> "
 dnf install nginx -y  &>>${log_file}
 
-print_head()  " <<<<<<< Removing old content >>>>>>> "
+print_head  " <<<<<<< Removing old content >>>>>>> "
 rm -rf /usr/share/nginx/html/*  &>>${log_file}
 
-print_head()  " <<<<<<< Downloading frontend package >>>>>>> "
+print_head  " <<<<<<< Downloading frontend package >>>>>>> "
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
 
-print_head()  " <<<<<<< Unzipping the frontend content >>>>>>> "
+print_head " <<<<<<< Unzipping the frontend content >>>>>>> "
 cd /usr/share/nginx/html  &>>${log_file}
 unzip /tmp/frontend.zip &>>${log_file}
 
-print_head()  " <<<<<<< cp roboshop.conf file to system >>>>>>> "
+print_head " <<<<<<< cp roboshop.conf file to system >>>>>>> "
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-print_head()  " <<<<<<< Enable the nginx >>>>>>> "
+print_head  " <<<<<<< Enable the nginx >>>>>>> "
 systemctl enable nginx &>>${log_file}
 
-print_head()  " <<<<<<< restart the nginx >>>>>>> "
+print_head  " <<<<<<< restart the nginx >>>>>>> "
 systemctl restart nginx &>>${log_file}
