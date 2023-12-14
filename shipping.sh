b09c2a1fa2b8374a9f01f931a84defb1b69d6003
shivacharan999 <charan02ch@gmail.com>
2023-12-14 08:53:55 -0800

    M

--- shipping.sh
+++ shipping.sh
@@ -2,7 +2,7 @@ source common.sh
 
 mysql_root_password=$1
 
-if [ -z "${mysql_root_password}" ]; then
+if [ -z ${mysql_root_password} ]; then
 echo -e "\e[31m Need Mysql Root Password\e[0m"
 exit 1
 
