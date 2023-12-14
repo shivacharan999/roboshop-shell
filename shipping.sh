2d15a3c812fd8637ef3d430b0895ec0320defeb4
shivacharan999 <charan02ch@gmail.com>
2023-12-14 08:57:39 -0800

    M

--- shipping.sh
+++ shipping.sh
@@ -2,9 +2,10 @@ source common.sh
 
 mysql_root_password=$1
 
-if [ -z ${mysql_root_password} ]; then
-echo -e "\e[31m Need Mysql Root Password\e[0m"
-exit 1
+if [ -z "${mysql_root_password}" ]; then
+   echo -e "\e[31m Need Mysql Root Password\e[0m"
+   exit 1
+fi
 
 component=shipping
 schema_setup="mysql"
