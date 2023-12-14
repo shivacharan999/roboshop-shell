0bddcfdd5eeb0d50fe562b71f65c77ea863d5756
shivacharan999 <charan02ch@gmail.com>
2023-12-13 22:25:10 -0800

    M

--- frontend.sh
+++ frontend.sh
@@ -2,25 +2,29 @@ code_dir=$(pwd)
 log_file=/tmp/robodhop.log
 rm-rf ${log_file}
 
-echo -e "\e[35m <<<<<<< Installing Nginx >>>>>>> \e[0m"
+print_head() {
+    echo  "\e[36m$1\e[0m"
+}
+
+print_head()  " <<<<<<< Installing Nginx >>>>>>> "
 dnf install nginx -y  &>>${log_file}
 
-echo -e "\e[35m <<<<<<< Removing old content >>>>>>> \e[0m"
+print_head()  " <<<<<<< Removing old content >>>>>>> "
 rm -rf /usr/share/nginx/html/*  &>>${log_file}
 
-echo -e "\e[35m <<<<<<< Downloading frontend package >>>>>>> \e[0m"
+print_head()  " <<<<<<< Downloading frontend package >>>>>>> "
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
 
-echo -e "\e[35m <<<<<<< Unzipping the frontend content >>>>>>> \e[0m"
+print_head()  " <<<<<<< Unzipping the frontend content >>>>>>> "
 cd /usr/share/nginx/html  &>>${log_file}
 unzip /tmp/frontend.zip &>>${log_file}
 
-echo -e "\e[35m <<<<<<< cp roboshop.conf file to system >>>>>>> \e[0m"
+print_head()  " <<<<<<< cp roboshop.conf file to system >>>>>>> "
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-echo -e "\e[35m <<<<<<< Enable the nginx >>>>>>> \e[0m"
+print_head()  " <<<<<<< Enable the nginx >>>>>>> "
 systemctl enable nginx &>>${log_file}
 
-echo -e "\e[35m <<<<<<< restart the nginx >>>>>>> \e[0m"
+print_head()  " <<<<<<< restart the nginx >>>>>>> "
 systemctl restart nginx &>>${log_file}
